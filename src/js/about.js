390e355a10bf10bac5719f9e690d1bab713820b1
Fedor Lukhmanov <fedor.lukhmanov@ya.ru>
2020-04-16 04:06:48 +0300

    Set up slider, removed overflow:hidden, improved README, fixed plugin versions

--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,9 @@
 import "../css/about.css";
+import Flickity from "flickity";
+
+let slider = document.querySelector(".slider");
+new Flickity(slider, {
+  // cellAlign: "left",
+  groupCells: true,
+  initialIndex: 3,
+});
